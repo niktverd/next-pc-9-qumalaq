ff72f2b38d7f3f63ae00b25011a04ea0d3bb1cda
Nikolay Tverdokhlebov <nn.tverd@gmail.com>
2025-01-25 15:37:17 +0500

    feat: moves on boards

--- src/board/index.tsx
+++ src/board/index.tsx
@@ -3,11 +3,66 @@ import {useState} from 'react';
 import './styles.css';
 
 export const Board = () => {
-    const [gameState, _setGameState] = useState([
-        9, 9, 9, 9, 9, 9, 9, 8, 10, 9, 9, 9, 9, 9, 9, 9, 9, 9, 0, 0,
+    const [gameState, setGameState] = useState([
+        9, 9, 9, 9, 9, 9, 9, 9, 1, 10, 9, 9, 9, 9, 9, 9, 9, 1, 0, 0,
     ]);
+    const [turn, setTurn] = useState<'light' | 'dark'>('light');
     const [bottomNotationValues] = useState([0, 1, 2, 3, 4, 5, 6, 7, 8]);
     const [topNotationValues] = useState([8, 7, 6, 5, 4, 3, 2, 1, 0]);
+
+    const handleClick = (index: number) => () => {
+        const rocks = gameState[index];
+        let tempState = [...gameState];
+
+        if (rocks < 1) {
+            return;
+        }
+
+        if (rocks === 1) {
+            if (index === 17) {
+                tempState = tempState.map((values, indexLocal) => {
+                    if (indexLocal === 0) {
+                        return values + 1;
+                    } else if (indexLocal === index) {
+                        return 0;
+                    } else {
+                        return values;
+                    }
+                });
+            } else {
+                tempState = tempState.map((values, indexLocal) => {
+                    if (indexLocal === index) {
+                        return 0;
+                    } else if (indexLocal === index + 1) {
+                        return values + 1;
+                    } else {
+                        return values;
+                    }
+                });
+            }
+        } else {
+            let localIndex = index;
+            tempState[localIndex] = 0;
+            for (let i = 0; i < rocks; i++) {
+                if (localIndex > 17) {
+                    localIndex = 0;
+                }
+
+                tempState[localIndex] = tempState[localIndex] + 1;
+                localIndex++;
+            }
+        }
+
+        setGameState(tempState);
+
+        if (turn === 'light') {
+            setTurn('dark');
+        } else {
+            setTurn('light');
+        }
+    };
+
+    console.log(turn);
 
     return (
         <div className="container">
@@ -24,7 +79,13 @@ export const Board = () => {
                     {topNotationValues.map((index) => {
                         const recalculatedIndex = index + 9;
                         return (
-                            <div className="otau" key={recalculatedIndex}>
+                            <div
+                                className="otau"
+                                key={recalculatedIndex}
+                                onClick={
+                                    turn === 'dark' ? handleClick(recalculatedIndex) : undefined
+                                }
+                            >
                                 {gameState[recalculatedIndex]}
                             </div>
                         );
@@ -34,7 +95,13 @@ export const Board = () => {
                     {bottomNotationValues.map((index) => {
                         const recalculatedIndex = index;
                         return (
-                            <div className="otau" key={recalculatedIndex}>
+                            <div
+                                className="otau"
+                                key={recalculatedIndex}
+                                onClick={
+                                    turn === 'light' ? handleClick(recalculatedIndex) : undefined
+                                }
+                            >
                                 {gameState[recalculatedIndex]}
                             </div>
                         );
